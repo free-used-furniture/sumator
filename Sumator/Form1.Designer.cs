070ce1ad7be25b191abac96c5d6cf3662890df03
IvaDeneva <156186907+IviAve@users.noreply.github.com>
2024-04-23 18:38:37 +0300

    sumator

---
+++ Sumator/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿
+namespace Sumator
+{
+    partial class SumatorNiki
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBoxSum1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.calculate = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBoxSum2 = new System.Windows.Forms.TextBox();
+            this.textBoxSum3 = new System.Windows.Forms.TextBox();
+            this.textBoxSum4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox1.Location = new System.Drawing.Point(5, 41);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(145, 41);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBoxSum1
+            // 
+            this.textBoxSum1.Location = new System.Drawing.Point(317, 38);
+            this.textBoxSum1.Name = "textBoxSum1";
+            this.textBoxSum1.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSum1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(116, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "+";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(275, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "=";
+            // 
+            // calculate
+            // 
+            this.calculate.Location = new System.Drawing.Point(5, 274);
+            this.calculate.Name = "calculate";
+            this.calculate.Size = new System.Drawing.Size(412, 45);
+            this.calculate.TabIndex = 5;
+            this.calculate.Text = "CALCULATE";
+            this.calculate.UseVisualStyleBackColor = true;
+            this.calculate.Click += new System.EventHandler(this.calculate_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(114, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(11, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "-";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(275, 105);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "=";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(116, 147);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(11, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "*";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(275, 147);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(13, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "=";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(275, 199);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(13, 13);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "=";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(116, 199);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(12, 13);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "/";
+            // 
+            // textBoxSum2
+            // 
+            this.textBoxSum2.Location = new System.Drawing.Point(318, 98);
+            this.textBoxSum2.Name = "textBoxSum2";
+            this.textBoxSum2.Size = new System.Drawing.Size(99, 20);
+            this.textBoxSum2.TabIndex = 18;
+            // 
+            // textBoxSum3
+            // 
+            this.textBoxSum3.Location = new System.Drawing.Point(317, 147);
+            this.textBoxSum3.Name = "textBoxSum3";
+            this.textBoxSum3.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSum3.TabIndex = 19;
+            // 
+            // textBoxSum4
+            // 
+            this.textBoxSum4.Location = new System.Drawing.Point(317, 196);
+            this.textBoxSum4.Name = "textBoxSum4";
+            this.textBoxSum4.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSum4.TabIndex = 20;
+            // 
+            // SumatorNiki
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ClientSize = new System.Drawing.Size(429, 331);
+            this.Controls.Add(this.textBoxSum4);
+            this.Controls.Add(this.textBoxSum3);
+            this.Controls.Add(this.textBoxSum2);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.calculate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxSum1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "SumatorNiki";
+            this.Text = "SumatorNiki";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBoxSum1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button calculate;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBoxSum2;
+        private System.Windows.Forms.TextBox textBoxSum3;
+        private System.Windows.Forms.TextBox textBoxSum4;
+    }
+}
+
---
+++ Sumator/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sumator
+{
+    public partial class SumatorNiki : Form
+    {
+        public SumatorNiki()
+        {
+            InitializeComponent();
+        }
+
+        private void calculate_Click(object sender, EventArgs e)
+        {
+            decimal num1 = decimal.Parse(this.textBox1.Text);
+            decimal num2 = decimal.Parse(this.textBox2.Text);
+            
+            
+
+            decimal sum1 = num1 + num2;
+            decimal sum2 = num1 - num2;
+            decimal sum3 = num1 * num2;
+            decimal sum4 = num1 / num2;
+
+            textBoxSum1.Text = sum1.ToString();
+            textBoxSum2.Text = sum2.ToString();
+            textBoxSum3.Text = sum3.ToString();
+            textBoxSum4.Text = sum4.ToString();
+        }
+    }
+}
